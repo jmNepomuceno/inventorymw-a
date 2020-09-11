51c584111a1e6c4b7c328286cf88e512d6282ef1
toastDerivation <jmgnngmj@gmail.com>
2020-09-11 08:13:20 +0800

    Updates

---
+++ precache-manifest.caddc9a9388cb3a916e312fedccb60a2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feae0f19f37273232ff11ed55b0a9fe6",
+    "url": "/inventorymw-a/index.html"
+  },
+  {
+    "revision": "6c14c2af6d6a5562b283",
+    "url": "/inventorymw-a/static/css/main.7930f4ac.chunk.css"
+  },
+  {
+    "revision": "7b0704e4a075bb354973",
+    "url": "/inventorymw-a/static/js/2.b9dd05d5.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/inventorymw-a/static/js/2.b9dd05d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c14c2af6d6a5562b283",
+    "url": "/inventorymw-a/static/js/main.936749bd.chunk.js"
+  },
+  {
+    "revision": "f294612f49b1df2279d7",
+    "url": "/inventorymw-a/static/js/runtime-main.762b3069.js"
+  },
+  {
+    "revision": "286ccc456ea700a27549a17b955f87e2",
+    "url": "/inventorymw-a/static/media/book.286ccc45.png"
+  },
+  {
+    "revision": "d7589e15b6e60c2ff64db93bbb1f1c7b",
+    "url": "/inventorymw-a/static/media/bpsuLogo.d7589e15.png"
+  },
+  {
+    "revision": "af31097379748617e1558535dc4746b8",
+    "url": "/inventorymw-a/static/media/clean.af310973.png"
+  },
+  {
+    "revision": "4ace96871ab275b5d5eb9a082c76b7cf",
+    "url": "/inventorymw-a/static/media/close.4ace9687.png"
+  },
+  {
+    "revision": "a528dbc30c125e9997c41b4df1268c22",
+    "url": "/inventorymw-a/static/media/collegeStudents.a528dbc3.jpg"
+  },
+  {
+    "revision": "859a07184a510649aed5d40e572e19b0",
+    "url": "/inventorymw-a/static/media/id.859a0718.png"
+  },
+  {
+    "revision": "b5c27f7995f6a982e44f6684c2bd1eba",
+    "url": "/inventorymw-a/static/media/inventory.b5c27f79.png"
+  },
+  {
+    "revision": "8a9e4d6217ccbdcb34df4f4cadcab0bf",
+    "url": "/inventorymw-a/static/media/inventoryBg.8a9e4d62.png"
+  },
+  {
+    "revision": "c1f4b308a79670138d28ab743ea5e993",
+    "url": "/inventorymw-a/static/media/login.c1f4b308.png"
+  },
+  {
+    "revision": "372a53631e20143553421a18738fc1a0",
+    "url": "/inventorymw-a/static/media/projector.372a5363.png"
+  },
+  {
+    "revision": "8feff93941a2425f1d7b98118625a17d",
+    "url": "/inventorymw-a/static/media/user.8feff939.png"
+  }
+]);
